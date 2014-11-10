7eb4a09311c9ff989fb9b46bd1e1df80f73d9e26
Matthew King <mking@crownchimp.com>
2014-11-10 13:35:14 -0700

    Added angular-sanitize to bower.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,6 +53,7 @@ module.exports = function (grunt) {
           'bower_components/jquery/dist/jquery.js'
           , 'bower_components/angular/angular.js'
           , 'bower_components/angular-route/angular-route.js'
+          , 'bower_components/angular-sanitize/angular-sanitize.js'
           , 'bower_components/bootstrap/dist/js/bootstrap.js'
           , 'modules/cafe-modules.js'
           , 'services/cafe-data-handler.js'
